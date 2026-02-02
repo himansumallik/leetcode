77316f91804fd58ed52834020314344d45c468f5
Himansu Mallik <dreamhimansu@gmail.com>
2026-02-02 22:47:27 +0530

    Time: 62 ms (65.82%) | Memory: 61.8 MB (80.00%) - LeetSync

---
+++ 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string maxSumOfSquares(int num, int sum) {
+        if (sum > 9 * num) return "";
+
+        string ans = "";
+
+        for (int i = 0; i < num; i++) {
+            int digit = min(9, sum);
+            ans.push_back('0' + digit);
+            sum -= digit;
+        }
+
+        return ans;
+    }
+};
