1ed74ab175297852cc42d90cb5d54bb91bda8061
Himansu Mallik <dreamhimansu@gmail.com>
2026-06-04 12:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (7.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;  
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] != 0) {
+        //         nums[j] = nums[i];
+        //         j++;
+        //     }
+        // }
+
+        // for (int i = j; i < nums.size(); i++) {
+        //     nums[i] = 0;
+        // }
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
