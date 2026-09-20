3b9d9b3c85457d289c07d7572792bba5d8268b9e
Himansu Mallik <dreamhimansu@gmail.com>
2026-09-20 22:20:08 +0530

    Time: 2 ms (77.53%) | Memory: 7.9 MB (37.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int fib(int n) {
-        // if(n<=1) return n;
+        if(n<=1) return n;
         // return fib(n-1)+fib(n-2);
 
 
-        if(n<=1) return n;
-        vector<int> arr(n+1);
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2; i<=n; i++){
-            arr[i]= arr[i-1] + arr[i-2];
+        // vector<int> arr(n+1);
+        // arr[0]=0;
+        // arr[1]=1;
+        // for(int i=2; i<=n; i++){
+        //     arr[i]= arr[i-1] + arr[i-2];
+        // }
+        // return arr[n];
+
+
+        int prev2 = 0;
+        int prev1 = 1;
+        for(int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = current;
         }
-        return arr[n];
+
+        return prev1;
     }
 };
