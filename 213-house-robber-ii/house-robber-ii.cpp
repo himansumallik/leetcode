6a4741a4b919bc0fbf7bb817b84bacdc70feeed2
Himansu Mallik <dreamhimansu@gmail.com>
2026-09-22 23:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (74.97%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -6,21 +6,21 @@ public:
         if(n==1) return nums[0];
         if(n==2) return max(nums[0], nums[1]);
 
-        // taking last, not first house (ranges from index 1 to n-1)
-        int lprev2 = nums[1];
-        int lprev1 = max(nums[1], nums[2]); // FIX 1: Must be max of the first two houses in this range
+        //taking last not first house
+        int lprev2=nums[1];
+        int lprev1=max(nums[1],nums[2]);
         for(int i=3; i<n; i++){
             int current = max(nums[i]+lprev2, lprev1);
-            lprev2 = lprev1;
+            lprev2=lprev1;
             lprev1 = current;
         }
 
-        // taking first, not last house (ranges from index 0 to n-2)
-        int fprev2 = nums[0];
-        int fprev1 = max(nums[0], nums[1]); // FIX 2: Must be max of the first two houses in this range
+        //taking first not last house
+        int fprev2=nums[0];
+        int fprev1=max(nums[0],nums[1]);
         for(int i=2; i<n-1; i++){
             int current = max(nums[i]+fprev2, fprev1);
-            fprev2 = fprev1;
+            fprev2=fprev1;
             fprev1 = current;
         }
 
